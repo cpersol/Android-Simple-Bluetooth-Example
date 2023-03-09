18df4cc611b7ab36ae673a7a2f19f8ca8ef0e8f6
cpersol <carlasimat@gmail.com>
2023-03-09 16:36:03 +0100

    FEAT: add functionality to generic button terminal

--- app/src/main/java/com/mcuhq/simplebluetooth/GenericFragment.java
+++ app/src/main/java/com/mcuhq/simplebluetooth/GenericFragment.java
@@ -1,10 +1,16 @@
 package com.mcuhq.simplebluetooth;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,42 +27,61 @@ public class GenericFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private BluetoothConnection mConnectionBT;
+    private String readMessage = null;
+    private Button mButtonTerminal;
+    private TextView mTerminal;
+    private String informacionRecibida;
     public GenericFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BoilerFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static GenericFragment newInstance(String param1, String param2) {
+   /* public static GenericFragment newInstance(String param1, String param2) {
         GenericFragment fragment = new GenericFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
+    }*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generic, container, false);
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_generic, container, false);
+        mConnectionBT = new BluetoothConnection(getActivity());
+
+        //mHandler= new HandleBluetooth(this);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (readMessage == null) {
+            Log.d("msg", "readMessage null");
+        }
+        informacionRecibida=mConnectionBT.lastMessage;
+        mTerminal=view.findViewById(R.id.terminalView) ;
+      //  mTerminal.setMovementMethod(new ScrollingMovementMethod());
+       // mTerminal.setScrollbarFadingEnabled(false);
+        mButtonTerminal=view.findViewById(R.id.terminalButton) ;
+
+
+        mButtonTerminal.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String mensaje = "Información recibida: " + informacionRecibida;
+                mTerminal.append(mensaje + "\n");
+
+            }
+        });
+
     }
 }
