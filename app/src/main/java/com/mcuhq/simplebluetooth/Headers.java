0397ca454997dc65348ae48307a5e243fc3e5cd2
cpersol <carlasimat@gmail.com>
2023-04-04 10:34:45 +0200

    REFACTOR: change name

--- app/src/main/java/com/mcuhq/simplebluetooth/Headers.java
+++ app/src/main/java/com/mcuhq/simplebluetooth/Headers.java
@@ -9,12 +9,15 @@ public class Headers {
     public static String SET_TIME = "";
     public static String GET_TIME = "";
     public static String GET_GENERIC_INFO="";
+    public static String GET_DEVICE_INFO="";
+
     public Headers() {
         this.GET_GENERIC_DATA = "00";
         this.GET_GENERIC_INFO = "A0";
         this.SET_APPKEY = "A1";
         this.SET_DEVEUI = "A2";
         this.SET_TIME = "A3";
+        this.GET_DEVICE_INFO = "B0";
 
 
 
