a0b3d78a3fd25d428d8f1c9cb95aa78608c033b4
cpersol <carlasimat@gmail.com>
2023-04-13 14:42:16 +0200

    REFACTOR :  changes so that you can check if the textView is empty

--- app/src/main/java/com/mcuhq/simplebluetooth/GenericFragment.java
+++ app/src/main/java/com/mcuhq/simplebluetooth/GenericFragment.java
@@ -43,7 +43,7 @@ public class GenericFragment extends Fragment {
     private String appkey_base64String;
     private String deveui_hexString;
     private String deveui_base64String;
-    private String interval_hexString;
+    private String interval_intString;
     private String interval_base64String;
     public Headers headers = new Headers();
     private String input_hexString;
@@ -53,13 +53,14 @@ public class GenericFragment extends Fragment {
     private TextView textViewINPUT;
 
 
-
     private TextView mTerminal;
     private String informacionRecibida;
     private MainActivity mainActivity;
-    public  String readMessage;
+    public String readMessage;
     private TextView gBluetoothStatus;
     private TextView gReadBuffer;
+    public String infoGeneric;
+
     public GenericFragment() {
         // Required empty public constructor
     }
@@ -81,121 +82,126 @@ public class GenericFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mTerminal=view.findViewById(R.id.terminalView) ;
-        buttonTerminal=view.findViewById(R.id.terminalButton) ;
-        buttonAppKey=view.findViewById(R.id.appKeyButton) ;
-        buttonDeveui=view.findViewById(R.id.deveuiButton) ;
-        buttonTimeTx=view.findViewById(R.id.txButton) ;
-        buttonInput=view.findViewById(R.id.inputButton) ;
-        appKeyText= view.findViewById(R.id.txtapp_key);
-        deveuiText= view.findViewById(R.id.txtdev_eui);
-        timeTxText= view.findViewById(R.id.txttime_tx);
-        inputText= view.findViewById(R.id.txtInputTerminal);
-        buttonGetConfigSensor= view.findViewById(R.id.getInfoButton);
-        textViewAPPKEY= view.findViewById(R.id.textViewAppKey);
-        textViewDEVEUI= view.findViewById(R.id.textViewDeveui);
-        textViewINTERVAL= view.findViewById(R.id.textViewInterval);
-        textViewINPUT= view.findViewById(R.id.textViewInput);
-        buttonTerminal.setOnClickListener(new View.OnClickListener(){
+        mTerminal = view.findViewById(R.id.terminalView);
+        buttonTerminal = view.findViewById(R.id.terminalButton);
+        buttonAppKey = view.findViewById(R.id.appKeyButton);
+        buttonDeveui = view.findViewById(R.id.deveuiButton);
+        buttonTimeTx = view.findViewById(R.id.txButton);
+        buttonInput = view.findViewById(R.id.inputButton);
+        appKeyText = view.findViewById(R.id.txtapp_key);
+        deveuiText = view.findViewById(R.id.txtdev_eui);
+        timeTxText = view.findViewById(R.id.txttime_tx);
+        inputText = view.findViewById(R.id.txtInputTerminal);
+        buttonGetConfigSensor = view.findViewById(R.id.getInfoButton);
+        textViewAPPKEY = view.findViewById(R.id.textViewAppKey);
+        textViewDEVEUI = view.findViewById(R.id.textViewDeveui);
+        textViewINTERVAL = view.findViewById(R.id.textViewInterval);
+        textViewINPUT = view.findViewById(R.id.textViewInput);
+        buttonTerminal.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                readMessage=MainActivity.getInstance().getBluetoothMessage();
+            public void onClick(View v) {
+                readMessage = MainActivity.getInstance().getBluetoothMessage();
                 String mensaje = "Información recibida: " + readMessage;
                 mTerminal.append(mensaje + "\n");
             }
         });
-        buttonAppKey.setOnClickListener(new View.OnClickListener(){
+        buttonAppKey.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(MainActivity.getInstance().mConnectionBT.mConnectedThread != null){//First check to make sure thread created
-                    appkey_hexString = Headers.SET_APPKEY+appKeyText.getText().toString();
-                    if(ProcessToSendMessage.isHexadecimal(appkey_hexString)){
-                    String appkey_base64String=ProcessToSendMessage.hexToBase64(appkey_hexString);
-                    MainActivity.getInstance().mConnectionBT.mConnectedThread.write(appkey_base64String);
-                    textViewAPPKEY.setText("");
-                    Toast.makeText(getActivity().getApplicationContext(), getString(R.string.sent), Toast.LENGTH_SHORT).show();
-                    }
-                    else{
+            public void onClick(View v) {
+                if (MainActivity.getInstance().mConnectionBT.mConnectedThread != null) {//First check to make sure thread created
+                    String appKeyEditText=appKeyText.getText().toString();
+                    appkey_hexString = Headers.SET_APPKEY + appKeyEditText;
+                    if (!appKeyEditText.isEmpty()){
+                    if (ProcessToSendMessage.isHexadecimal(appkey_hexString)) {
+                        String appkey_base64String = ProcessToSendMessage.hexToBase64(appkey_hexString);
+                        MainActivity.getInstance().mConnectionBT.mConnectedThread.write(appkey_base64String);
+                        appKeyText.setText("");
+                        Toast.makeText(getActivity().getApplicationContext(), getString(R.string.sent), Toast.LENGTH_SHORT).show();
+                    } else {
                         textViewAPPKEY.setText(MainActivity.getInstance().mConnectionBT.errorTextHexFormat);
 
                     }
-            }}
-        });
-        buttonDeveui.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                if(MainActivity.getInstance().mConnectionBT.mConnectedThread != null) //First check to make sure thread created
-                    deveui_hexString = Headers.SET_DEVEUI+deveuiText.getText().toString();
-                if(ProcessToSendMessage.isHexadecimal(deveui_hexString)){
-                        String deveui_base64String=ProcessToSendMessage.hexToBase64(deveui_hexString);
-                        MainActivity.getInstance().mConnectionBT.mConnectedThread.write( deveui_base64String);
-                        textViewDEVEUI.setText("");
-                        Toast.makeText(getActivity().getApplicationContext(), getString(R.string.sent), Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        textViewDEVEUI.setText(MainActivity.getInstance().mConnectionBT.errorTextHexFormat);
 
-                    }
-
+                }}
             }
         });
-        buttonTimeTx.setOnClickListener(new View.OnClickListener(){
+        buttonDeveui.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(MainActivity.getInstance().mConnectionBT.mConnectedThread != null) //First check to make sure thread created
-                    interval_hexString = Headers.SET_TIME+timeTxText.getText().toString();
-                if(ProcessToSendMessage.isHexadecimal(interval_hexString)){
-                    String interval_base64String=ProcessToSendMessage.hexToBase64(interval_hexString);
-                    MainActivity.getInstance().mConnectionBT.mConnectedThread.write(interval_base64String);
-                    textViewINTERVAL.setText("");
+            public void onClick(View v) {
+                if (MainActivity.getInstance().mConnectionBT.mConnectedThread != null) {//First check to make sure thread created
+                   String deveuiEditText=deveuiText.getText().toString();
+                    deveui_hexString = Headers.SET_DEVEUI + deveuiEditText;
+                if (!deveuiEditText.isEmpty()){
+                if (ProcessToSendMessage.isHexadecimal(deveui_hexString)) {
+                    String deveui_base64String = ProcessToSendMessage.hexToBase64(deveui_hexString);
+                    MainActivity.getInstance().mConnectionBT.mConnectedThread.write(deveui_base64String);
+                    deveuiText.setText("");
                     Toast.makeText(getActivity().getApplicationContext(), getString(R.string.sent), Toast.LENGTH_SHORT).show();
-                }  else{
-                    textViewINTERVAL.setText(MainActivity.getInstance().mConnectionBT.errorTextHexFormat);
+                } else {
+                    textViewDEVEUI.setText(MainActivity.getInstance().mConnectionBT.errorTextHexFormat);
 
                 }
-            }
+
+            }}}
         });
-        buttonInput.setOnClickListener(new View.OnClickListener(){
+        buttonTimeTx.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(MainActivity.getInstance().mConnectionBT.mConnectedThread != null)//First check to make sure thread created
-                    input_hexString=inputText.getText().toString();
-                if(ProcessToSendMessage.isHexadecimal(input_hexString)) {
-                        String input_base64String=ProcessToSendMessage.hexToBase64(input_hexString);
-                        MainActivity.getInstance().mConnectionBT.mConnectedThread.write(input_base64String);
-                        textViewINPUT.setText("");
-                    }else{
-                        textViewINPUT.setText(MainActivity.getInstance().mConnectionBT.errorTextHexFormat);
+            public void onClick(View v) {
+                if (MainActivity.getInstance().mConnectionBT.mConnectedThread != null){//First check to make sure thread created
+                    String timeTxEditText=timeTxText.getText().toString();
+                    interval_intString = Headers.SET_TIME + timeTxEditText;
+                    if(!timeTxEditText.isEmpty()){
+                MainActivity.getInstance().mConnectionBT.mConnectedThread.write(interval_intString);
+                timeTxText.setText("");
+                Toast.makeText(getActivity().getApplicationContext(), getString(R.string.sent), Toast.LENGTH_SHORT).show();
+                //textViewINTERVAL.setText(MainActivity.getInstance().mConnectionBT.errorTextHexFormat);
+            }}}
+        });
+
+        buttonInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (MainActivity.getInstance().mConnectionBT.mConnectedThread != null)//First check to make sure thread created
+                    input_hexString = inputText.getText().toString();
+                if(!input_hexString.isEmpty()){
+                if (ProcessToSendMessage.isHexadecimal(input_hexString)) {
+                    String input_base64String = ProcessToSendMessage.hexToBase64(input_hexString);
+                    MainActivity.getInstance().mConnectionBT.mConnectedThread.write(input_base64String);
+                    textViewINPUT.setText("");
+                } else {
+                    textViewINPUT.setText(MainActivity.getInstance().mConnectionBT.errorTextHexFormat);
+                }
+
+            }}
+        });
+        buttonGetConfigSensor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (MainActivity.getInstance().mConnectionBT.mConnectedThread != null) {
+                    String infoSensor = MainActivity.getInstance().getBluetoothMessage();
+                    Log.d("infoSensor", "readMessage");
+                    String[] partes = infoSensor.split("_");
+
+
+                    String header = partes[0];
+
+                    if (header.equals(headers.GET_GENERIC_INFO)) {
+                        Log.d("infoSensor", "Split");
+                        String appKey = partes[1];
+                        String deveui = partes[2];
+                        String time = partes[3];
+
+                        appKeyText.setText(appKey);
+                        deveuiText.setText(deveui);
+                        timeTxText.setText(time);
+
                     }
-
-            }
-        });
-        buttonGetConfigSensor.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                if(MainActivity.getInstance().mConnectionBT.mConnectedThread != null){
-                    String infoSensor =MainActivity.getInstance().getBluetoothMessage();
-                Log.d("infoSensor", "readMessage");
-                String [] partes = infoSensor.split("_");
-
-
-                String header = partes[0];
-
-                if(header.equals(headers.GET_GENERIC_INFO)) {
-                    Log.d("infoSensor", "Split");
-                    String appKey = partes[1];
-                    String deveui = partes[2];
-                    String time = partes[3];
-
-                    appKeyText.setText(appKey);
-                    deveuiText.setText(deveui);
-                    timeTxText.setText(time);
-
-                }}else{
+                } else {
                     Toast.makeText(getActivity().getApplicationContext(), getString(R.string.sBTdevNF), Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
 
     }
 }
